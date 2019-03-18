d9d7bb5761d87f0c3fe60760c1fbb5f2e3c38e94
Irving Duran <ijd.irving@gmail.com>
2019-03-18 19:58:45 -0300

    card clickable

--- Project-I-Social-Card/social-card/src/App.js
+++ Project-I-Social-Card/social-card/src/App.js
@@ -1,15 +1,16 @@
 import React from "react";
 import "./App.css";
-import HeaderContainer from './components/HeaderComponents/HeaderContainer';
-import CardContainer from './components/CardComponents/CardContainer';
-
+import HeaderContainer from "./components/HeaderComponents/HeaderContainer";
+import CardContainer from "./components/CardComponents/CardContainer";
 
 const App = () => {
   return (
-    <div className="app">
-    <HeaderContainer />
-    <CardContainer />
-    </div>
+    <a href="https://www.reacts.org">
+      <div className="app">
+        <HeaderContainer />
+        <CardContainer />
+      </div>
+    </a>
   );
 };
 
